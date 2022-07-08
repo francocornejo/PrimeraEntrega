23c44bf0732f4a2fdddfc6319ea45896a9fc724a
Cornejo Franco David <fcornejo@andreani.com>
2022-07-07 23:39:52 -0300

    second commit

--- Routes/index.js
+++ Routes/index.js
@@ -4,16 +4,16 @@ const { getProduct, postProduct, borrarProd, modificarProd} = require('../public
 const { postCarrito, insertProductoByIdToCart, verCarrito, deleteCarrito, deleteProductoCarrito  } = require('../public/controllers/controllerCart')
 
 //Routers de los Productos
-router.get( '/api/productos/:id?', getProduct)
-router.post( '/api/productos', postProduct)
-router.put('/api/productos/:id', modificarProd)
-router.delete('/api/productos/:id', borrarProd)
+router.get( '/api/productos/:id?', getProduct )
+router.post( '/api/productos', postProduct )
+router.put('/api/productos/:id', modificarProd )
+router.delete('/api/productos/:id', borrarProd )
 
 //Routers del Carrito
-router.post('/api/carrito', postCarrito)
-router.post('/api/carrito/:id/productos', insertProductoByIdToCart)
+router.post('/api/carrito', postCarrito )
+router.post('/api/carrito/:id/productos', insertProductoByIdToCart )
 router.get('/api/carrito/:id/productos', verCarrito )
 router.delete('/api/carrito/:id', deleteCarrito )
-router.delete('/api/carrito/:id/productos/:id_prod', deleteProductoCarrito)
+router.delete('/api/carrito/:id/productos/:id_prod', deleteProductoCarrito )
 
 module.exports = router
